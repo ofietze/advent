10df3432eb84f869e0bb18e824fb25d13ff94c7f
ofietze <f.oliver@live.de>
2023-12-09 10:24:50 +0100

    Add basic typescript structure

--- 1/solution.ts
+++ 1/solution.ts
@@ -6,3 +6,4 @@
 // In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
 
 // Consider your entire calibration document. What is the sum of all of the calibration values?
+console.log("advent");

eaf232702785b8710ef3fd64b328676314510c3b
ofietze <f.oliver@live.de>
2023-12-09 10:05:58 +0100

    init

---
+++ 1/solution.ts
@@ -0,0 +1,8 @@
+// 1abc2
+// pqr3stu8vwx
+// a1b2c3d4e5f
+// treb7uchet
+
+// In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
+
+// Consider your entire calibration document. What is the sum of all of the calibration values?

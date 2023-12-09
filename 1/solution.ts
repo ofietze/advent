733372ae3cb8cb2f7356ddeba67b094b6be08e75
ofietze <f.oliver@live.de>
2023-12-09 11:09:26 +0100

    Add first solution, update dependencies
    Solved calibration value exercise

--- 1/solution.ts
+++ 1/solution.ts
@@ -1,3 +1,4 @@
+import fs from "fs";
 // 1abc2
 // pqr3stu8vwx
 // a1b2c3d4e5f
@@ -6,4 +7,28 @@
 // In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
 
 // Consider your entire calibration document. What is the sum of all of the calibration values?
-console.log("advent");
+const getCalibrationValue = (line: string): number => {
+  let first = "";
+  let latestMatch = "";
+  for (const char of line) {
+    if (char.match(/[0-9]/)) {
+      latestMatch = char;
+      if (first.length === 0) {
+        first = char;
+      }
+    }
+  }
+  let twoDigits = first.concat(latestMatch);
+  const result = twoDigits.length === 2 ? Number.parseInt(twoDigits) : 0;
+  return result;
+};
+
+const fileContent = fs.readFileSync("1/puzzleInput.txt", "utf8");
+const lines = fileContent.split("\n");
+let sum = 0;
+
+for (const line of lines) {
+  sum += getCalibrationValue(line);
+}
+
+console.log(sum);
